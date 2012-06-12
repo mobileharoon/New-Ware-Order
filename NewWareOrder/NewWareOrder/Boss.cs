6c26ea1d7dd01657798f897f74c75b58490d9f49
Haroon Khalid <mobileharoon@gmail.com>
2012-06-12 15:51:15 -0400

    first commit

---
+++ NewWareOrder/NewWareOrder/Boss.cs
@@ -0,0 +1,97 @@
+﻿// Projectile.cs
+//Using declarations
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NewWareOrder
+{
+    class Boss
+    {
+        // Image representing the Projectile
+        public Texture2D Texture;
+
+        // Position of the Projectile relative to the upper left side of the screen
+        public Vector2 Position;
+
+        // State of the Projectile
+        public bool Active;
+
+        // The amount of damage the projectile can inflict to an enemy
+        public int Damage;
+
+        // Represents the viewable boundary of the game
+        Viewport viewport;
+
+        public int boundry;
+
+        // direction
+        String direction = "right";
+
+        // Get the width of the projectile ship
+        public int Width
+        {
+            get { return Texture.Width; }
+        }
+
+        // Get the height of the projectile ship
+        public int Height
+        {
+            get { return Texture.Height; }
+        }
+
+        // Determines how fast the projectile moves
+        float projectileMoveSpeed;
+
+
+        public void Initialize(Viewport viewport, Texture2D texture, Vector2 position, int boundry_coord)
+        {
+            Texture = texture;
+            Position = position;
+            this.viewport = viewport;
+
+            Active = true;
+
+            Damage = 2;
+
+            projectileMoveSpeed = 40f;
+
+            boundry = boundry_coord;
+            
+
+        }
+        public void Update()
+        {
+            // Projectiles always move to the right
+            //Position.X -= 1;
+
+            if (Position.X > (this.boundry - Texture.Width))
+            {
+                direction = "left";
+            }
+
+            if (Position.X < 0)
+            {
+                direction = "right";
+            }
+
+
+            if (direction == "right")
+            {
+                Position.X += 1;
+            }
+
+            if (direction == "left")
+            {
+                Position.X -= 1;
+            }
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+            //spriteBatch.Draw(Texture, Position, null, Color.White, 0f, new Vector2(Width / 2, Height / 2), 1f, SpriteEffects.None, 0f);
+        
+        }
+    }
+}
---
+++ NewWareOrder/NewWareOrder/Enemy.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NewWareOrder
+{
+    class Enemy
+    {
+
+        // Animation representing the enemy
+        public Animation EnemyAnimation;
+
+        // The position of the enemy ship relative to the top left corner of thescreen
+        public Vector2 Position;
+
+        public Vector2 delta;
+
+        // The state of the Enemy Ship
+        public bool Active;
+
+        // The hit points of the enemy, if this goes to zero the enemy dies
+        public int Health;
+
+        // The amount of damage the enemy inflicts on the player ship
+        public int Damage;
+
+        // The amount of score the enemy will give to the player
+        public int Value;
+
+        // Get the width of the enemy ship
+        public int Width
+        {
+            get { return EnemyAnimation.FrameWidth; }
+        }
+
+        // Get the height of the enemy ship
+        public int Height
+        {
+            get { return EnemyAnimation.FrameHeight; }
+        }
+
+        // The speed at which the enemy moves
+        float enemyMoveSpeed;
+
+        public void Initialize(Animation animation, Vector2 position, Vector2 player_pos)
+        {
+            // Load the enemy ship texture
+            EnemyAnimation = animation;
+
+            // Set the position of the enemy
+            Position = position;
+
+
+            // We initialize the enemy to be active so it will be update in the game
+            Active = true;
+
+
+            // Set the health of the enemy
+            Health = 1;
+
+            // Set the amount of damage the enemy can do
+            Damage = 10;
+
+            // Set how fast the enemy moves
+            enemyMoveSpeed = 4f;
+
+
+            // Set the score value of the enemy
+            Value = 100;
+
+        }
+
+
+        public void Update(GameTime gameTime, int boundry, Vector2 player_pos, Vector2 boss_pos)
+        {
+            // The enemy always moves to the left so decrement it's xposition
+            //Position.Y += enemyMoveSpeed;
+
+            // Get the enemy to go towards the player
+            Vector2 delta;
+            delta = player_pos - Position;
+            delta.Normalize();
+           
+            /*
+            double delta_len;
+            delta_len = Math.Sqrt((delta.X * delta.X) + (delta.Y * delta.Y));
+
+            Position.X = delta.X * (20 / (float)delta_len);
+            Position.Y = delta.Y * (20 / (float)delta_len);
+            */
+
+            Position += delta * 4;
+
+           //  var distance = player.position - this.position; 
+            //distance.Normalize(); 
+            //this.position += distance * enemy_speed * delta_time;
+
+            // Update the position of the Animation
+            EnemyAnimation.Position = Position;
+
+            // Update Animation
+            EnemyAnimation.Update(gameTime);
+
+            // If the enemy is past the screen or its health reaches 0 then deactivateit
+            if (Position.Y > boundry  || Health <= 0)
+            {
+                // By setting the Active flag to false, the game will remove this objet fromthe 
+                // active game list
+                Active = false;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the animation
+            EnemyAnimation.Draw(spriteBatch);
+        }
+    }
+}
